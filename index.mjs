711ac7633dcf67f634fe9d5811b1d250f51287e1
Kishan Gajera <me@kgajera.com>
2022-11-12 22:50:33 -0500

    fix: harden logic to get next migration version

--- index.mjs
+++ index.mjs
@@ -19,39 +19,54 @@ try {
   if (stdout.trim() === "-- This is an empty migration.") {
     console.log(chalk.green("No migrations are needed"));
   } else {
-    const {
-      configManager: { current },
-    } = await loadConfig({}, "");
+    const migrationsDir = await getMigrationsDir();
+    const nextMigrationVersion = await getNextMigrationVersion(migrationsDir);
+    const migrationFilename = getMigrationFilename(nextMigrationVersion, description);
+    const migrationPath = path.resolve(migrationsDir, migrationFilename);
 
-    const migrationsDir = current.migrations?.dir;
-
-    if (!fs.existsSync(migrationsDir)) {
-      throw new Error(`Migrations directory does not exist: ${migrationsDir}`);
-    }
-
-    const migrations = fs.readdirSync(migrationsDir);
-    const migrationNumber = migrations.length
-      ? Number(migrations[migrations.length - 1].split(".")[0]) + 1
-      : 1;
-
-    const padding =
-      migrationNumber < 10 ? "00" : migrationNumber < 100 ? "0" : "";
-
-    const migrationPath = path.resolve(
-      migrationsDir,
-      `${padding}${migrationNumber}.do.${description?.length ? `${description}.` : ""
-      }sql`
-    );
-
-    fs.writeFileSync(migrationPath, stdout);
+    await fs.promises.writeFile(migrationPath, stdout);
 
     console.log(
-      `${chalk.green("Generated migration file:")} ${chalk.underline(
-        migrationPath
-      )}`
+      `${chalk.green("Generated migration file:")} ${chalk.underline(migrationPath)}`
     );
   }
 } catch (error) {
   console.log(chalk.red(error.message));
   process.exitCode = 1;
+}
+
+/**
+ * Returns `migrations.dir` value from the Platformatic DB configuration file
+ */
+async function getMigrationsDir() {
+  const { configManager: { current } } = await loadConfig({}, "");
+  const migrationsDir = current.migrations?.dir;
+
+  if (!fs.existsSync(migrationsDir)) {
+    throw new Error(`Migrations directory does not exist: ${migrationsDir}`);
+  }
+
+  return migrationsDir;
+}
+
+/**
+ * Returns a valid Postgrator migration filename
+ * https://github.com/rickbergfalk/postgrator#usage
+ */
+function getMigrationFilename(version, description = "") {
+  return `${version}.do.${description?.length ? `${description}.` : ""}sql`;
+}
+
+/**
+ * Returns the the next migration version number
+ */
+async function getNextMigrationVersion(migrationsDir) {
+  const migrations = await fs.promises.readdir(migrationsDir)
+  const sortedMigrations = migrations.filter(file => path.extname(file) === ".sql").sort();
+
+  const versionNumber = sortedMigrations.length
+    ? Number(sortedMigrations[sortedMigrations.length - 1].split(".")[0]) + 1
+    : 1;
+
+  return versionNumber.toString().padStart(3, "0");
 }
